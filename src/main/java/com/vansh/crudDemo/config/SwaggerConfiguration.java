60a8558f5bc7008541202023bc556477b0283033
Vansh.Garg <Vansh.Garg@Geminisolutions.com>
2022-08-08 13:32:42 +0530

    Removed commented code

--- src/main/java/com/vansh/crudDemo/config/SwaggerConfiguration.java
+++ src/main/java/com/vansh/crudDemo/config/SwaggerConfiguration.java
@@ -20,26 +20,5 @@ public class SwaggerConfiguration {
                         .description("SpringShop Wiki Documentation")
                         .url("https://springshop.wiki.github.org/docs"));
     }
-
-    //    @Bean
-//    public Docket productApi() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.vansh.crudDemo"))
-//                .paths(PathSelectors.any())
-//                .build()
-//                .apiInfo(getInfo());
-//    }
-//
-//    private ApiInfo getInfo() {
-//        return new ApiInfo(
-//                "My First SpringBoot CRUD Application",
-//                "Spring Boot Crud Application designed to access different Course Details.",
-//                "1.0",
-//                "Terms of service",
-//                new Contact("Vansh Garg", "https://localhost:8080/courses/", "vansh.garg@geminisolutions.com"),
-//                "Apache License Version 2.0",
-//                "Apache License", new ArrayList<>());
-//    }
 }
 
